29b3012a8626b2a1c5909b3319085ce904bc5aff
elinalakshya <elinalakshya@gmail.com>
2025-12-14 17:03:41 +0530

    Update worker.py

--- worker.py
+++ worker.py
@@ -1,3 +1,14 @@
+import os
+import sys
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+SRC_PATH = os.path.join(BASE_DIR, "src")
+
+if SRC_PATH not in sys.path:
+    sys.path.insert(0, SRC_PATH)
+
+print("🔧 SRC_PATH =", SRC_PATH)
+
 # worker.py
 
 import os
